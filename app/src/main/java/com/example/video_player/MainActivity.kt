0528a22a805217e5d129b4f1d1551b5c6e3ea9e2
danilosoarespinheiro <danilosoarespinheiro@gmail.com>
2023-11-25 21:42:20 -0400

    Created a video player
    - Added Video view on content_main.xml
    - Imported MediaControlled and Uri to use VideoView
    - Added a sample.mp4 file to test the video player

--- app/src/main/java/com/example/video_player/MainActivity.kt
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.video_player
 
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.video_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +20,20 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val videoView = binding.mainContentLayout.testView
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        //specify the location of video file
+        val uri: Uri = parse("android.resource://$packageName/raw/sample")
+
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,11 +50,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
